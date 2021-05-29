6baf92a22aabe2855be1085387bcdba182d749e0
ivanjermakov <ivanjermakov1@gmail.com>
2021-05-29 14:53:07 +0300

    Apply data

--- src/app/component/day/day.component.ts
+++ src/app/component/day/day.component.ts
@@ -14,10 +14,31 @@ export class DayComponent implements OnInit {
     @Input()
     size: number = 12
 
+    @Input()
+    showDate: boolean = false
+
+    color: string | undefined
+    textColor: string = 'hsl(27, 0%, 90%)'
+
     constructor() {
     }
 
     ngOnInit(): void {
+        switch (this.day!.value) {
+            case 0:
+                this.color = 'hsl(27, 0%, 90%)'
+                this.textColor = 'hsl(27, 0%, 40%)'
+                break
+            case 1:
+                this.color = 'hsl(27, 48%, 70%)'
+                break
+            case 2:
+                this.color = 'hsl(27, 48%, 50%)'
+                break
+            case 3:
+                this.color = 'hsl(27, 48%, 35%)'
+                break
+        }
     }
 
 }
